b5ce7b4eb076fac9511fc3573b8c2e4e19773879
codelighter <shb8845369@gmail.com>
2018-08-02 18:56:47 +0800

    add remove baidu ads.

--- js/background.js
+++ js/background.js
@@ -1,5 +1,6 @@
 chrome.contextMenus.create({
-    title: "火箭博客",
-    onclick: function(){alert('火箭博客');},
-	documentUrlPatterns: ['https://blog.csdn.net/*','https://www.cnblogs.com/*'] // 只在某些页面显示此右键菜单
+    title: "广告猎手",
+    onclick: function(){alert('广告猎手');},
+	documentUrlPatterns: ['https://blog.csdn.net/*',
+	'https://www.cnblogs.com/*'] // 只在某些页面显示此右键菜单
 });
