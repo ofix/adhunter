49561e0ac9fa98b8df397c033b4131e3a2abee83
codelighter <shb8845369@gmail.com>
2018-08-18 16:03:41 +0800

    add mode-change button

--- js/common.js
+++ js/common.js
@@ -39,6 +39,70 @@ function killAdTimeout(flag){
         }
     },600);
 }
+var gEmitCss = 0;
+var gEmitJs = 0;
+var gEmitHtml = 0;
+//发射代码
+function emitCode(code,ele){
+    $(ele).append(code);
+}
+function compile(code,template){
+    for(var i=0,l=code.length+1; i<l; i++){
+        pattern = /#\d+/;
+        template = template.replace(pattern,code[i]);
+    }
+    return template;
+}
+function emitCss_cnblogs(css){
+    if(gEmitCss>0){
+        return;
+    }
+    gEmitCss++;
+    var template = '<style type="text/css">#1</style>';
+    var code = [
+       `.compile-mode:{
+            width:200px;
+            height:40px;
+            text-align:center;
+            position:fixed;
+            right:20px;
+            bottom:20px;
+        }`
+    ];
+    var css = document.createElement('style');
+    css.type = 'text/css';
+    css.appendChild(document.createTextNode(code.join(' ')));
+    $(document.head).append(css);
+}
+
+function emitJs_cnblogs(js,ele){
+    if(gEmitJs>0){
+        return;
+    }
+    gEmitJs++;
+    var code = [
+         `$(document).click(function(){
+            var ele = ["#header","#right","#mystats",
+            "#mylinks","#bnr_pic","#sideBar",
+            "#MySignature","#blog_post_info_block",".postDesc",
+            "#comment_form","#footer","#blog-comments-placeholder"];
+            ele.forEach(function(v,i,a){
+                $(v).show();
+            });
+         });`
+    ];
+    var js = document.createElement('script');
+    js.textContent = code.join(' ');
+    document.body.appendChild(js);
+}
+function emitHtml_cnblogs(){
+    if(gEmitHtml>0){
+        return;
+    }
+    gEmitHtml++;
+    var template = '<div class="compile-mode">切换模式</div>';
+    $(document.body).append(template);
+}
 //CSDN
 function csdn(){
 	var o = [".meau-list","header","#csdn-toolbar","aside",".comment-box",".recommend-box",".t0",".related-article",
@@ -47,8 +111,15 @@ function csdn(){
 }
 //博客园
 function cnblogs(){
-	var o = ["#header","#sideBar","#MySignature","#blog_post_info_block",".postDesc","#comment_form","#footer","#blog-comments-placeholder"];
+	var o = ["#header","#right","#mystats","#mylinks","#bnr_pic","#sideBar","#MySignature","#blog_post_info_block",".postDesc","#comment_form","#footer","#blog-comments-placeholder"];
 	removeAds(o);
+	$("#left").css({"left":"0px","top":"0px"});
+	$(".post").css({'border':"none;"});
+    $("#mainContent").css({"margin-left":"150px","margin-right":"150px"});
+    $("body").css({"background":""});
+    emitHtml_cnblogs();
+    emitCss_cnblogs();
+    emitJs_cnblogs();
 }
 //yiibai
 function yiibai(){
